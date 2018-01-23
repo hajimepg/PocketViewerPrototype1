54f1ed7df2056ce67eee6aac187002e42d66a6e7
Kenichi Saito <hajime@studiohff.net>
2018-01-23 23:43:26 +0900

    neDbArticleRepository.findUnreadのテストを追加

--- test/ndDbArticleRepoistoryTest.ts
+++ test/ndDbArticleRepoistoryTest.ts
@@ -1,5 +1,6 @@
 import test from "ava";
 
+import Article from "../src/main/model/article";
 import NeDbArticleRepository from "../src/main/repository/neDbArticleRepository";
 
 test.beforeEach((t) => {
@@ -15,4 +16,16 @@ test.serial(async (t) => {
     t.is(0, article.tags.length);
     t.true(article.isUnread);
     t.false(article.isFavorite);
+});
+
+test.serial(async (t) => {
+    await t.context.repo.insert("title1", "http://example.com", [], true, false);
+    await t.context.repo.insert("title2", "http://example.com", [], false, false);
+    await t.context.repo.insert("title3", "http://example.com", [], true, false);
+
+    const articles: Article[] = await t.context.repo.findUnread();
+    t.is(2, articles.length);
+    const titles = articles.map((article) => article.title);
+    t.not(-1, titles.indexOf("title1"));
+    t.not(-1, titles.indexOf("title3"));
 });
