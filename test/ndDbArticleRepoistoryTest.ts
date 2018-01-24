3d3fd6850e9f2dbf37084314230ac67a63ed9c6c
Kenichi Saito <hajime@studiohff.net>
2018-01-25 00:45:09 +0900

    NeDbArticleRepositoryのユニットテスト(ひとまず完成)

--- test/ndDbArticleRepoistoryTest.ts
+++ test/ndDbArticleRepoistoryTest.ts
@@ -83,3 +83,114 @@ test.serial("findUnread", async (t) => {
     t.not(-1, titles.indexOf("title1"));
     t.not(-1, titles.indexOf("title3"));
 });
+
+test.serial("findByTag", async (t) => {
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+
+    await repo.insert("title1", "http://example.com", "example.com", [], true, false);
+    await repo.insert("title2", "http://example.com", "example.com", ["Game"], true, false);
+    await repo.insert("title3", "http://example.com", "example.com", ["Game", "Programming"], true, false);
+
+    {
+        const articles = await repo.findByTag("Game");
+        t.is(2, articles.length);
+        const titles = articles.map((article) => article.title);
+        t.not(-1, titles.indexOf("title2"));
+        t.not(-1, titles.indexOf("title3"));
+    }
+
+    {
+        const articles = await repo.findByTag("Programming");
+        t.is(1, articles.length);
+        const titles = articles.map((article) => article.title);
+        t.not(-1, titles.indexOf("title3"));
+    }
+
+    {
+        const articles = await repo.findByTag("Anime");
+        t.is(0, articles.length);
+    }
+});
+
+test.serial("findHosts", async (t) => {
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+
+    {
+        const hosts = await repo.findHosts();
+        t.is(0, hosts.length);
+    }
+
+    await repo.insert("title", "http://example.com", "example.com", [], true, false);
+
+    {
+        const hosts = await repo.findHosts();
+        t.is(1, hosts.length);
+        t.not(-1, hosts.indexOf("example.com"));
+    }
+
+    await repo.insert("title", "http://example.net", "example.net", [], true, false);
+
+    {
+        const hosts = await repo.findHosts();
+        t.is(2, hosts.length);
+        t.not(-1, hosts.indexOf("example.com"));
+        t.not(-1, hosts.indexOf("example.net"));
+    }
+
+    await repo.insert("title", "http://example.com/another", "example.com", [], true, false);
+
+    {
+        const hosts = await repo.findHosts();
+        t.is(2, hosts.length);
+        t.not(-1, hosts.indexOf("example.com"));
+        t.not(-1, hosts.indexOf("example.net"));
+    }
+});
+
+test.serial("findTags", async (t) => {
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+
+    {
+        const tags = await repo.findTags();
+        t.is(0, tags.length);
+    }
+
+    await repo.insert("title", "http://example.com", "example.com", [], true, false);
+
+    {
+        const tags = await repo.findTags();
+        t.is(0, tags.length);
+    }
+
+    await repo.insert("title", "http://example.com", "example.com", ["tag1"], true, false);
+
+    {
+        const tags = await repo.findTags();
+        t.is(1, tags.length);
+        t.not(-1, tags.indexOf("tag1"));
+    }
+
+    await repo.insert("title", "http://example.com", "example.com", ["tag2", "tag3"], true, false);
+
+    {
+        const tags = await repo.findTags();
+        t.is(3, tags.length);
+        t.not(-1, tags.indexOf("tag1"));
+        t.not(-1, tags.indexOf("tag2"));
+        t.not(-1, tags.indexOf("tag3"));
+    }
+
+    await repo.insert("title", "http://example.com", "example.com", ["tag3", "tag4"], true, false);
+
+    {
+        const tags = await repo.findTags();
+        t.is(4, tags.length);
+        t.not(-1, tags.indexOf("tag1"));
+        t.not(-1, tags.indexOf("tag2"));
+        t.not(-1, tags.indexOf("tag3"));
+        t.not(-1, tags.indexOf("tag4"));
+    }
+});
