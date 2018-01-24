383cc59c75e9c159d8ab2226341cb48e262d80ca
Kenichi Saito <hajime@studiohff.net>
2018-01-25 00:27:04 +0900

    Articleクラスのid以外は変更可能に

--- src/main/model/article.ts
+++ src/main/model/article.ts
@@ -1,12 +1,12 @@
 export default class Article {
     public constructor(
         public readonly id: string,
-        public readonly title: string,
-        public readonly url: string,
-        public readonly host: string,
-        public readonly tags: string[],
-        public readonly isUnread: boolean,
-        public readonly isFavorite: boolean
+        public title: string,
+        public url: string,
+        public host: string,
+        public tags: string[],
+        public isUnread: boolean,
+        public isFavorite: boolean
     ) {
     }
 }
