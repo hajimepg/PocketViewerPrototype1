d5da06111420ec5d1deb6b27d298d0e83c124018
Kenichi Saito <hajime@studiohff.net>
2018-01-25 00:15:22 +0900

    t.context.repoは型定義情報が消えてしまっていて辛いので使わなくする

--- test/ndDbArticleRepoistoryTest.ts
+++ test/ndDbArticleRepoistoryTest.ts
@@ -3,15 +3,19 @@ import test from "ava";
 import Article from "../src/main/model/article";
 import NeDbArticleRepository from "../src/main/repository/neDbArticleRepository";
 
-test.beforeEach((t) => {
-    t.context.repo  = new NeDbArticleRepository();
-    t.context.repo.init();
-    t.context.repo.deleteAll();
+test.beforeEach(async (t) => {
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+    repo.deleteAll();
 });
 
 test.serial(async (t) => {
-    const article: Article = await t.context.repo.insert("title", "http://example.com", "example.com",
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+
+    const article = await repo.insert("title", "http://example.com", "example.com",
         ["Game", "Programming"], true, false);
+
     t.true(article.id.length > 0);
     t.is("title", article.title);
     t.is("http://example.com", article.url);
@@ -24,7 +28,10 @@ test.serial(async (t) => {
 });
 
 test.serial(async (t) => {
-    const article: Article = await t.context.repo.insert("title", "http://example.com", "example.com",
+    const repo = new NeDbArticleRepository();
+    await repo.init();
+
+    const article = await repo.insert("title", "http://example.com", "example.com",
         ["Game", "Programming"], true, false);
 
     article.url = "http://example.net";
@@ -33,7 +40,7 @@ test.serial(async (t) => {
     article.isUnread = false;
     article.isFavorite = true;
 
-    const updatedArticle: Article = await t.context.repo.update(article);
+    const updatedArticle = await repo.update(article);
     t.is(article.id, updatedArticle.id);
     t.is(article.url, updatedArticle.url);
     t.is(article.host, updatedArticle.host);
@@ -43,19 +50,19 @@ test.serial(async (t) => {
 });
 
 test.serial(async (t) => {
-    const article1: Article = await t.context.repo.insert("title1", "http://example.com", "example.com",
-        [], true, false);
-    const article2: Article = await t.context.repo.insert("title2", "http://example.com", "example.com",
-        [], true, false);
-    const article3: Article = await t.context.repo.insert("title3", "http://example.com", "example.com",
-        [], true, false);
+    const repo = new NeDbArticleRepository();
+    await repo.init();
 
-    const allArticlesBefore: Article[] = await t.context.repo.findAll();
+    const article1 = await repo.insert("title1", "http://example.com", "example.com", [], true, false);
+    const article2 = await repo.insert("title2", "http://example.com", "example.com", [], true, false);
+    const article3 = await repo.insert("title3", "http://example.com", "example.com", [], true, false);
+
+    const allArticlesBefore = await repo.findAll();
     t.is(3, allArticlesBefore.length);
 
-    await t.context.repo.delete(article2);
+    await repo.delete(article2);
 
-    const allArticlesAfter: Article[] = await t.context.repo.findAll();
+    const allArticlesAfter = await repo.findAll();
     t.is(2, allArticlesAfter.length);
     const ids = allArticlesAfter.map((article) => article.id);
     t.not(-1, ids.indexOf(article1.id));
@@ -63,11 +70,14 @@ test.serial(async (t) => {
 });
 
 test.serial(async (t) => {
-    await t.context.repo.insert("title1", "http://example.com", "example.com", [], true, false);
-    await t.context.repo.insert("title2", "http://example.com", "example.com", [], false, false);
-    await t.context.repo.insert("title3", "http://example.com", "example.com", [], true, false);
+    const repo = new NeDbArticleRepository();
+    await repo.init();
 
-    const articles: Article[] = await t.context.repo.findUnread();
+    await repo.insert("title1", "http://example.com", "example.com", [], true, false);
+    await repo.insert("title2", "http://example.com", "example.com", [], false, false);
+    await repo.insert("title3", "http://example.com", "example.com", [], true, false);
+
+    const articles = await repo.findUnread();
     t.is(2, articles.length);
     const titles = articles.map((article) => article.title);
     t.not(-1, titles.indexOf("title1"));
